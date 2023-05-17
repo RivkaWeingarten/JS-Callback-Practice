8795ec55b7d893259ea267cba8e48ab3035ec90e
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-05-17 14:04:14 -0400

    Added the offscreen logic

--- move.js
+++ move.js
@@ -2,10 +2,12 @@ function move(element) {
     element.style.position = 'fixed'
 
     function moveToCoordinates(left, bottom) {
+        
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
         element.style.zIndex = 1
-    }
+    
+}
 
     function moveWithArrowKeys(left, bottom, callback){
         let direction = null;
@@ -14,26 +16,42 @@ function move(element) {
     
         element.style.left = x + 'px'
         element.style.bottom = y + 'px'
+       
+        viewportHeight = window.innerHeight;
+        viewportWidth = window.innerWidth;
+        console.log(viewportHeight, 'h x', viewportWidth, 'w');
         
         function moveCharacter(){ 
-            if(direction === 'west'){
+            if
+            (direction === 'west')
+                {if (x>10){
                 x-=1
             }
+        }
             if(direction === 'north'){
+                if(y<viewportHeight-50){
                 y+=1
             }
+        }
             if(direction === 'east'){
+                if(x <viewportWidth-50){
                 x+=1
             }
+        }
             if(direction === 'south'){
+                if(y>70){
                 y-=1
             }
+        }
             element.style.left = x + 'px'
             element.style.bottom = y + 'px'
         }
-        
+        // if element is off vw or vh then ignore
+
+       
+           
         setInterval(moveCharacter, 1)
-        
+            
         document.addEventListener('keydown', function(e){
             if(e.repeat) return;
         
